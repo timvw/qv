29229872f91e5a1f7775bf51220e162b440b7cea
Tim Van Wassenhove <tim@timvw.be>
2022-09-22 19:18:31 +0200

    tech: run lint

--- src/globbing_path.rs
+++ src/globbing_path.rs
@@ -119,8 +119,10 @@ fn extract_path_parts(path_with_scheme: &str) -> Result<(ObjectStoreUrl, Path, O
         Url::parse(non_globbed_path).map_err(|e| DataFusionError::External(Box::new(e)))?;
     let (object_store_url, prefix) = match non_globbed_url.scheme() {
         "file" => ObjectStoreUrl::parse("file://").map(|osu| (osu, non_globbed_url.path())),
-        _ => ObjectStoreUrl::parse(&non_globbed_url[url::Position::BeforeScheme..url::Position::BeforePath])
-            .map(|osu| (osu, non_globbed_url.path())),
+        _ => ObjectStoreUrl::parse(
+            &non_globbed_url[url::Position::BeforeScheme..url::Position::BeforePath],
+        )
+        .map(|osu| (osu, non_globbed_url.path())),
     }?;
 
     let prefix_without_leading_delimiter = prefix.strip_prefix('/').unwrap_or(prefix);
