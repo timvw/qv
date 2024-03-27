a43fe3876472d0fe97662dbf6170606bc42f4e3f
Tim Van Wassenhove <tim@timvw.be>
2024-03-27 16:23:30 +0100

    feat: add support for aws s3 console url

--- src/args.rs
+++ src/args.rs
@@ -4,8 +4,6 @@ use chrono::{DateTime, Utc};
 use clap::Parser;
 use datafusion::common::{DataFusionError, Result};
 use regex::Regex;
-use std::collections::HashMap;
-use url::Url;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -137,48 +135,4 @@ fn test_parse_glue_url() {
         Some(("db".to_string(), "table".to_string())),
         parse_glue_url("glue://db.table")
     );
-}
-
-/// When the provided s looks like an https url from the amazon webui convert it to an s3:// url
-/// When the provided s does not like such url, return it as is.
-#[allow(dead_code)]
-fn update_s3_console_url(s: &str) -> (bool, String) {
-    if s.starts_with("https://s3.console.aws.amazon.com/s3/buckets/") {
-        let parsed_url = Url::parse(s).unwrap_or_else(|_| panic!("Failed to parse {}", s));
-        let path_segments = parsed_url
-            .path_segments()
-            .map(|c| c.collect::<Vec<_>>())
-            .unwrap_or_default();
-        if path_segments.len() == 3 {
-            let bucket_name = path_segments[2];
-            let params: HashMap<String, String> = parsed_url
-                .query()
-                .map(|v| {
-                    url::form_urlencoded::parse(v.as_bytes())
-                        .into_owned()
-                        .collect()
-                })
-                .unwrap_or_default();
-            params
-                .get("prefix")
-                .map(|prefix| format!("s3://{}/{}", bucket_name, prefix))
-                .map(|x| (true, x))
-                .unwrap_or_else(|| (false, s.to_string()))
-        } else {
-            (false, s.to_string())
-        }
-    } else {
-        (false, s.to_string())
-    }
-}
-
-#[test]
-fn test_update_s3_console_url() -> Result<()> {
-    assert_eq!(
-        update_s3_console_url("/Users/timvw/test"),
-        (false, "/Users/timvw/test".to_string())
-    );
-    assert_eq!(update_s3_console_url("https://s3.console.aws.amazon.com/s3/buckets/datafusion-delta-testing?region=eu-central-1&prefix=COVID-19_NYT/&showversions=false"), (true, "s3://datafusion-delta-testing/COVID-19_NYT/".to_string()));
-    assert_eq!(update_s3_console_url("https://s3.console.aws.amazon.com/s3/buckets/datafusion-delta-testing?prefix=COVID-19_NYT/&region=eu-central-1"), (true, "s3://datafusion-delta-testing/COVID-19_NYT/".to_string()));
-    Ok(())
 }
